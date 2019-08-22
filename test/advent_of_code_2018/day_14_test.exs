15d02712b92504e96a8c69828af7e369ed0a94b2
davvidbaker <davvidbaker@gmail.com>
2019-08-22 09:29:59 -0600

    test?

--- test/advent_of_code_2018/day_14_test.exs
+++ test/advent_of_code_2018/day_14_test.exs
@@ -3,12 +3,12 @@ defmodule AdventOfCode2018.Day14Test do
 
   import AdventOfCode2018.Day14
 
-  @tag :skip
+  # @tag :skip
   test "part1" do
-    {:ok, io} = StringIO.open()
-    result = part1(IO.stream(io, :line))
-
-    assert result
+    assert part1(9) == 5_158_916_779
+    assert part1(5) == 0_124_515_891
+    assert part1(18) == 9_251_071_085
+    assert part1(2018) == 5_941_429_882
   end
 
   @tag :skip
